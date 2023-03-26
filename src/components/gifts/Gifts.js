7001dd655b8409076aa6fbc1211c467f4a80e6ee
abdulkareemAbunabhan <zman17881@gmail.com>
2023-03-26 10:07:43 +0000

    done the Gift companent and modal

--- src/components/gifts/Gifts.js
+++ src/components/gifts/Gifts.js
@@ -1,5 +1,29 @@
-export default function (){
-    return(
-        <div> <p>hi gifts</p></div>
-    )
+import axios from "axios";
+// import Card from 'react-bootstrap/Card'
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Gift from "./Gift";
+import { useEffect,useState } from "react";
+function Gifts() {
+    const [datafromApi, setDatafromApi] = useState([]);
+  const serverUrl = "https://back-end-production-7df4.up.railway.app/gifts";
+  const sendReq= ()=>{axios.get(serverUrl).then((resp) => {
+    setDatafromApi(resp.data)
+})};
+useEffect(()=>{
+    sendReq()
+},[]);
+
+  return (
+    <Row xs={1} md={4} className="g-4">
+      {datafromApi.map((item) => {
+        return (
+          <Col key={item.gift_title}>
+            <Gift item={item} />
+          </Col>
+        );
+      })}
+    </Row>
+  );
 }
+export default Gifts;
