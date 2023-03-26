a3d6e741c7b745ef0e3e23ab0a47d0c69160eddb
abdullahshaghnoba <abdullahshagnoba@gmail.com>
2023-03-26 15:37:23 +0000

    new setup for react app added after raaa`s meeting

--- src/components/yourevent/flower2/FlowerYourEvent.js
+++
@@ -1,7 +0,0 @@
-export default function FlowerYourEvent (){
-    return (
-        <div>
-            
-        </div>
-    )
-}
--- src/components/yourevent/gifts2/GiftsYourEvent.js
+++
@@ -1,7 +0,0 @@
-export default function GiftsYourEvent (){
-    return (
-        <div>
-           hi 
-        </div>
-    )
-}
