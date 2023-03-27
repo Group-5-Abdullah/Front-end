8a9a8c218a75ca8d46c7049b88ecdace1cd6a6c3
AbdelrahmanElatrash <a.elatrash170@gmail.com>
2023-03-27 02:54:30 +0000

    fix delet button in event food

--- src/components/yourevent/food/FoodYourEvent.js
+++ src/components/yourevent/food/FoodYourEvent.js
@@ -5,62 +5,71 @@ import Row from 'react-bootstrap/Row';
 import Button from 'react-bootstrap/Button';
 // import axios from 'axios';
 
-function useForceUpdate(){
-    const [value, setValue] = useState(0); 
+function useForceUpdate() {
+    const [value, setValue] = useState(0);
     return () => setValue(value => value + 1)
 }
 export default function FoodYourEvent(props) {
-    console.log(props)
-    console.log(props.clickedEvent.eventid);
+    
     const [foodArray, setFoodArray] = useState([]);
-        const serverURL = `${process.env.REACT_APP_serverURL}food?eventid=${props.clickedEvent.eventid}`
-        console.log(serverURL)
-        const fetchData = () => {
-            fetch(serverURL)
-                .then(res => res.json())
-                .then(data =>{
-                    setFoodArray(data)
+    const forceUpdate = useForceUpdate();
+    const serverURL = `${process.env.REACT_APP_serverURL}food?eventid=${props.clickedEvent.eventid}`;
     
-                } )
-    
-                .catch((err) => {
-                    console.log(err);
-                })
-    
-        }
+    console.log(serverURL)
+    const fetchData = () => {
+        fetch(serverURL)
+            .then(res => res.json())
+            .then(data => {
+                setFoodArray(data)
 
-        // axios.get(serverURL)
-        // .then((result) => {
-        //     let foodData = result.data
-        //     setFoodArray(foodData);
-        //     console.log(foodData)
-        // })
-        // .catch((err) => {
-        //     console.log(err);
-        //   });
+            })
 
-    
-    const forceUpdate = useForceUpdate()
-    useEffect(() => {
-        fetchData();
-        forceUpdate()
-    }, [])
-    
-    console.log(foodArray)
-    return (
-        <Row xs={1} md={4} className="g-4">
-            {foodArray.map((item) => {
-                return (
-                    <Col key={item.id} >
-                        <Card style={{ width: '18rem', backgroundColor: 'gray' }}>
-                            <Card.Img variant="top" src={`${item.food_image}`} />
-                            <Card.Title>{item.food_title}</Card.Title>
+            .catch((err) => {
+                console.log(err);
+            })
 
-                            <Button variant="primary" onClick="">delete </Button>
+    }
 
-                        </Card>
-                    </Col>)
-            })}
-        </Row>
-    )
+// .....      delete food   .......................................................
+       const deleteFood = (id) => {
+        console.log(id);
+        const deleteURL = `${process.env.REACT_APP_serverURL}food?id=${id}&eventid=${props.clickedEvent.eventid}` //&eventid=${props.clickedEvent.eventid}&user_email=${localStorage.getItem('user_email')}`
+        // const deleteURL = `${process.env.REACT_APP_serverURL}food?eventid=${id}&user_email=${localStorage.getItem('user_email')}`
+        fetch(deleteURL, { method: 'DELETE' })
+            .then((res) => {
+                res.json()
+            })
+            .then(() => fetchData())
+
+            .catch((err) => {
+                console.log(err);
+            })
+
+    }
+
+
+
+
+useEffect(() => {
+    fetchData();
+    forceUpdate()
+}, [])
+
+console.log(foodArray)
+return (
+    <Row xs={1} md={4} className="g-4">
+        {foodArray.map((item) => {
+            return (
+                <Col key={item.id} >
+                    <Card style={{ width: '18rem', backgroundColor: 'gray' }}>
+                        <Card.Img variant="top" src={`${item.food_image}`} />
+                        <Card.Title>{item.food_title}</Card.Title>
+
+                        <Button variant="danger" onClick={()=>{deleteFood(item.id)}}>delete </Button>
+
+                    </Card>
+                </Col>)
+        })}
+    </Row>
+)
 }
